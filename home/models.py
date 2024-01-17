90db02e9fc201d2f3819c5d064796de9baf799a7
ogsahil <contactsahilpnayak@gmail.com>
2024-01-17 16:03:06 +0530

    created a superuser

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    tasktitle = models.CharField(max_length=80)
+    taskdesc = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
