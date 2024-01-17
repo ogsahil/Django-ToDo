04ee0ad605dc972fc188b46cdc2d79e2d5c63207
ogsahil <contactsahilpnayak@gmail.com>
2024-01-17 18:55:19 +0530

    added some features

--- home/models.py
+++ home/models.py
@@ -6,3 +6,7 @@ class Task(models.Model):
     tasktitle = models.CharField(max_length=80)
     taskdesc = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tasktitle
+    
--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from home.models import Task
 # Create your views here.
 
 def home(request):
+    if request.method == "POST":
+        #handle the form
+        title = request.POST['title']
+        desc = request.POST['desc']
+        print(title, desc)
+        ins = Task(tasktitle = title , taskdesc = desc)
+        ins.save()
     return render(request, 'home.html')
 
 
