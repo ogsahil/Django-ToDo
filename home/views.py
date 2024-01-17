a8f64c797aec5444fc93ad67f2a624baca958aba
ogsahil <contactsahilpnayak@gmail.com>
2024-01-17 14:32:12 +0530

    added some templates and form to add the task

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
